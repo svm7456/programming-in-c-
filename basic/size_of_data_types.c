87b984490a4a96be592ba1c74ddc52ba6960f7e8
svm7456 <111221987+svm7456@users.noreply.github.com>
2022-08-14 14:10:59 +0530

    Create size_of_data_types.c

---
+++ basic/size_of_data_types.c
@@ -0,0 +1,15 @@
+// This program prints size of all data types
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    char b;
+    float c;
+    double d;
+    printf("Siz of int is : %d\n", sizeof(a));
+    printf("Siz of char is : %d\n", sizeof(b));
+    printf("Siz of float is : %d\n", sizeof(c));
+    printf("Siz of double is : %d\n", sizeof(d));
+    return 0;
+}
